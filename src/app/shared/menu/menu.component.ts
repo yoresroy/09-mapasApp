08e026397cc3b302314ab6a37a227a263916cc72
RodrigoMegasys <rodrigo.ruiz@megasys.com.mx>
2023-04-05 16:49:32 -0600

    Add - Menu

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+
+interface MenuItem {
+  ruta: string;
+  nombre: string;
+}
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  menuItem : MenuItem[] = [
+    {
+      ruta : '/mapas/fullscreen',
+      nombre: 'FullScreen'
+    },
+    {
+      ruta : '/mapas/zoom-range',
+      nombre: 'Zoom Range'
+    }
+    ,
+    {
+      ruta : '/mapas/marcadores',
+      nombre: 'Marcadores'
+    }
+    ,
+    {
+      ruta : '/mapas/propiedades',
+      nombre: 'Propiedades'
+    }
+  ]
+
 
 }
