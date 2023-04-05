2c5f487e94fa2a1237edbd31df55cd3612ef05f2
RodrigoMegasys <rodrigo.ruiz@megasys.com.mx>
2023-04-05 16:32:26 -0600

    ADD - Estructura del proyecto

---
+++ src/app/mapas/mapas-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FullScreenComponent } from './components/full-screen/full-screen.component';
+import { ZoomRangeComponent } from './components/zoom-range/zoom-range.component';
+import { MarcadoresComponent } from './components/marcadores/marcadores.component';
+import { PropiedadesComponent } from './components/propiedades/propiedades.component';
+
+const routes: Routes = [
+
+  {
+    path : '',
+    children :[
+      { path: 'fullscreen', component: FullScreenComponent },
+      { path: 'zoom-range', component: ZoomRangeComponent },
+      { path: 'marcadores', component: MarcadoresComponent },
+      { path: 'propiedades', component: PropiedadesComponent },
+      { path: '**', redirectTo: 'fullscreen' }
+    ]
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class MapasRoutingModule { }
