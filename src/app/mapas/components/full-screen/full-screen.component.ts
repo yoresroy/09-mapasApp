c6b0c1c148a3b71ec44d04745c8608bc8c42c0ef
RodrigoMegasys <rodrigo.ruiz@megasys.com.mx>
2023-04-05 17:27:52 -0600

    Add - Mapa fullscreen, requiere token para funcionar*

--- src/app/mapas/components/full-screen/full-screen.component.ts
+++ src/app/mapas/components/full-screen/full-screen.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-full-screen',
@@ -10,6 +12,17 @@ export class FullScreenComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+
+    mapboxgl.accessToken = environment.mapboxToken;
+    var map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11'
+    });
+
+
+
   }
 
 }
